725bdcd00d825ba1d8c2241c7384f564b5cd2655
niceiyke <niceiyke04@gmail.com>
2024-02-29 05:54:02 +0100

    update

--- tradingBot/tradingbot.py
+++ tradingBot/tradingbot.py
@@ -2,28 +2,28 @@ import os
 from pybit.unified_trading import HTTP
 import pandas as pd
 from macd_strategy import get_strategy
-from helper import set_mode, get_pnl, get_positions, get_precisions,get_tp_spl
+from helper import set_mode, get_pnl, get_positions, get_precisions, get_tp_spl
 from strategy import three_moving_average_rsi_strategy
 from time import sleep
 
 
 class TradingBot:
     client = HTTP(
-    testnet=True,
-    api_key="vHnoV1kQTgYsmhaY2s",
-    api_secret="C5lVp013avzhEZgojyYxiT4TzwnZOX70gu8n",
-)
+        testnet=True,
+        api_key="vHnoV1kQTgYsmhaY2s",
+        api_secret="C5lVp013avzhEZgojyYxiT4TzwnZOX70gu8n",
+    )
     LEVERAGE = 10
     TIMEFRAME = 15
     CANDLESIZE = 750
     AMOUNT = 100
-    EXPECTED_PROFIT =50
+    EXPECTED_PROFIT = 20
     TAKE_PROFIT_MULTIPLIER = 3
     STOP_LOSS_MULTIPLIER = 2
     MARKET_SLEEP_TIME = 2
     MIN_SYMBOL_TURNOVER = 5000000
-    MIN_SYMBOL_24H_PCNT=0
-    MAX_OPEN_POSITION =10
+    MIN_SYMBOL_24H_PCNT = 0
+    MAX_OPEN_POSITION = 10
 
     def get_symbols(self):
         try:
@@ -34,7 +34,7 @@ class TradingBot:
                 if "USDT" in elem["symbol"]
                 and "USDC" not in elem["symbol"]
                 and float(elem["turnover24h"]) > self.MIN_SYMBOL_TURNOVER
-                and float(elem['price24hPcnt'])>self.MIN_SYMBOL_24H_PCNT
+                and float(elem["price24hPcnt"]) > self.MIN_SYMBOL_24H_PCNT
             ]
             print(f"You have total of {len(symbols)} crypto to trade")
             return symbols
@@ -58,19 +58,24 @@ class TradingBot:
         except Exception as err:
             print(f"Error fetching klines for {symbol}:", err)
 
-    def place_order_market(self, symbol, side,):
+    def place_order_market(
+        self,
+        symbol,
+        side,
+    ):
         try:
-            open_position =get_positions(client=self.client)
+            open_position = get_positions(client=self.client)
 
-
-            if len(open_position)==5:
+            if len(open_position) == 5:
                 return
-            
+
             if symbol in open_position:
                 return
-            
-            print("open position are",open_position)
-            price_precision, quantity_precision = get_precisions(client=self.client,symbol=symbol)
+
+            print("open position are", open_position)
+            price_precision, quantity_precision = get_precisions(
+                client=self.client, symbol=symbol
+            )
             sleep(self.MARKET_SLEEP_TIME)
 
             mark_price = float(
@@ -86,9 +91,13 @@ class TradingBot:
 
             sleep(self.MARKET_SLEEP_TIME)
 
-            tp,sl=get_tp_spl(expected_profit=self.EXPECTED_PROFIT ,amount=amount,price=mark_price,side=side,precision=price_precision)
-
-
+            tp, sl = get_tp_spl(
+                expected_profit=self.EXPECTED_PROFIT,
+                amount=amount,
+                price=mark_price,
+                side=side,
+                precision=price_precision,
+            )
 
             resp = self.client.place_order(
                 category="linear",
@@ -119,19 +128,19 @@ class TradingBot:
                     print(f"No dataframe available for {symbol}.")
                     continue
 
-                #strategy = get_strategy(df=df)
+                # strategy = get_strategy(df=df)
                 strategy = three_moving_average_rsi_strategy(df=df)
                 if strategy == "hold":
                     print(f"No strategy found for {symbol}.")
                     continue
                 elif strategy == "buy":
-                    self.place_order_market(side='buy',symbol=symbol)
+                    self.place_order_market(side="buy", symbol=symbol)
                 else:
-                    self.place_order_market(side='sell',symbol=symbol)
+                    self.place_order_market(side="sell", symbol=symbol)
         except Exception as err:
             print("Error in signal processing:", err)
 
-    def place_dummy_order(self, side,symbol):
+    def place_dummy_order(self, side, symbol):
         if side == "buy":
             print(f"long order placed {symbol}")
 
